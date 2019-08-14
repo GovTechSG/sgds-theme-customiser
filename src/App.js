c97415d18d7e3f681874359a3381803220463a44
Albert <albert_LI_from.NSEARCH@tech.gov.sg>
2019-08-14 10:52:04 +0800

    install missing packages and remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -66,61 +66,6 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Header />
-        {/* 
-        <MainNav>
-          <MainNavBrand>
-            <MainNavItem href="/">
-              <img src="https://www.designsystem.gov.sg/assets/img/logo_sgds.png" alt="..."></img>
-            </MainNavItem>
-            <MainNavBurger isActive={this.state.isActive} onClick={this.onClickBurger}>
-          </MainNavBrand>
-          <MainNavMenu isActive={this.state.isActive}>
-            <MainNavStart>
-              <MainNavItem href="#" onClick="() => this.displayComponent('customiser'))">Customiser</MainNavItem>
-              <MainNavItem href="#" onClick="() => this.displayComponent('template')"></MainNavItem>
-              <MainNavItem hasDropdown isHoverable href="#" title="A Dropdown Menu">
-                <MainNavDropdown>
-                  <MainNavItem href='#/'>One A</MainNavItem>
-                  <MainNavItem href='#/'>Two B</MainNavItem>
-                  <MainNavDivider />
-                  <MainNavItem href='#/'>Two A</MainNavItem>
-                </MainNavDropdown>
-              </MainNavItem>
-            </MainNavStart>
-            <MainNavEnd>
-              <MainNavItem hasDropdown isHoverable isMega href="#" title="A Mega Dropdown">
-                <MainNavDropdown>
-                  <div className="sgds-container">
-                    <div className="row">
-                      <div className="col is-3">
-                        <MainNavItem subMenu>Sub Menu 1></MainNavItem>
-                        <MainNavItem href="#">Sub-item 1a</MainNavItem>
-                        <MainNavItem href="#">Sub-item 1b</MainNavItem>
-                        <MainNavItem href="#">Sub-item 1c</MainNavItem>
-                      </div>
-                      <div className="col is-3">
-                        <MainNavItem subMenu>Sub Menu 2></MainNavItem>
-                        <MainNavItem href="#">Sub-item 2a</MainNavItem>
-                        <MainNavItem href="#">Sub-item 2b</MainNavItem>
-                        <MainNavItem href="#">Sub-item 2c</MainNavItem>
-                      </div>
-                      <div className="col is-6">
-                        <MainNavItem>
-                          <p>
-                              <b>Information</b><br />
-                              You can put short paragraph
-                              of information here to
-                              describe about this section.
-                          </p>
-                        </MainNavItem>
-                      </div>
-                    </div>
-                </MainNavDropdown>
-              </MainNavItem>
-            </MainNavEnd>
-          </MainNavMenu>
-        </MainNav>
-        */}
         <MainNav
           brand={mainNavItems.brand}
           links={mainNavItems.links}
