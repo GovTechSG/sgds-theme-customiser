cc4f7dfd124134feb8b0c38fc5861c98be9a393e
Sophia Chow <sophia.chow.2020@scis.smu.edu.sg>
2022-07-13 15:25:50 +0800

    remove template view

--- src/App.js
+++ src/App.js
@@ -21,16 +21,10 @@ import "./App.scss";
 const store = configureStore();
 
 const App = () => {
-  const [active, setActive] = useState(0)
-
-  const clickNavItem = (eventKey) => {
-    setActive(eventKey)
-  }
-
   return (
     <Provider store={store}>
       <SgdsMasthead />
-      <Navbar expand="md">
+      <Navbar className="justify-content-between">
         <Navbar.Brand href="/">
           <img
             src="https://www.designsystem.gov.sg/assets/img/logo-sgds.svg"
@@ -38,34 +32,13 @@ const App = () => {
             width="150"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="mainNav" />
-        <Navbar.Collapse id="mainNav">
-          <Nav className="me-auto" activeKey={active}>
-            <Nav.Item>
-              <Nav.Link
-                onClick={() => clickNavItem(0)}
-                eventKey="0"
-              >
-                Customisor View
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link
-                onClick={() => clickNavItem(1)}
-                eventKey="1"
-              >
-                Template View
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <div className="d-flex align-items-center">
-            <Button size="sm" href="https://v2dev.designsystem.gov.sg/" target="_blank">
-              SGDS Docs
-            </Button>
-          </div>
-        </Navbar.Collapse>
+        <div className="d-flex align-items-center">
+          <Button size="sm" href="https://v2dev.designsystem.gov.sg/" target="_blank">
+            SGDS Docs
+          </Button>
+        </div>
       </Navbar>
-      <Body display={active} />
+      <Body />
       <FooterComponent />
     </Provider>
   );
--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 // import elements
 import ComponentList from "../componentList/componentList";
-import Template from "../template/template";
 import Sidebar from "../sideBar/sideBar";
 
-const Body = (props) => {
+const Body = () => {
   return (
     <div className="appBody content-page">
-      {props.display === 0 ? <ComponentList /> : <Template />}
+      <ComponentList />
       <Sidebar />
     </div>
   );
