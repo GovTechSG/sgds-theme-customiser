1aba4ab1b03b7d515f92d240552375d5396f5286
Sophia Chow <sophia.chow.2020@scis.smu.edu.sg>
2022-07-13 13:54:50 +0800

    change app, body and component list to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 // redux
 import configureStore from "./store/configureStore";
@@ -20,69 +20,55 @@ import "./App.scss";
 // Register Store
 const store = configureStore();
 
-class App extends Component {
-  state = {
-    showPanel: 0,
-    showNavMenu: false,
-  };
+const App = () => {
+  const [active, setActive] = useState(0)
 
-  selectMenuItem = (item) => {
-    if (item === "Template View") {
-      this.setState({
-        showPanel: 1,
-      });
-    } else if (item === "Customiser View") {
-      this.setState({
-        showPanel: 0,
-      });
-    }
-  };
-
-  render() {
-    return (
-      <Provider store={store}>
-        {/* <Masthead hasMaxWidth={false}/> */}
-        <SgdsMasthead />
-        <Navbar expand="md">
-          <Navbar.Brand href="/">
-            <img
-              src="https://www.designsystem.gov.sg/assets/img/logo-sgds.svg"
-              alt="Main Logo"
-              width="150"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="mainNav" />
-          <Navbar.Collapse id="mainNav">
-            <Nav className="me-auto" activeKey={this.state.showPanel}>
-              <Nav.Item>
-                <Nav.Link
-                  onClick={() => this.selectMenuItem("Customiser View")}
-                  eventKey="0"
-                >
-                  Customisor View
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link
-                  onClick={() => this.selectMenuItem("Template View")}
-                  eventKey="1"
-                >
-                  Template View
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <div className="d-flex align-items-center">
-              <Button size="sm" href="https://v2dev.designsystem.gov.sg/" target="_blank">
-                SGDS Docs
-              </Button>
-            </div>
-          </Navbar.Collapse>
-        </Navbar>
-        <Body display={this.state.showPanel} />
-        <FooterComponent />
-      </Provider>
-    );
+  const clickNavItem = (eventKey) => {
+    setActive(eventKey)
   }
+
+  return (
+    <Provider store={store}>
+      <SgdsMasthead />
+      <Navbar expand="md">
+        <Navbar.Brand href="/">
+          <img
+            src="https://www.designsystem.gov.sg/assets/img/logo-sgds.svg"
+            alt="Main Logo"
+            width="150"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="mainNav" />
+        <Navbar.Collapse id="mainNav">
+          <Nav className="me-auto" activeKey={active}>
+            <Nav.Item>
+              <Nav.Link
+                onClick={() => clickNavItem(0)}
+                eventKey="0"
+              >
+                Customisor View
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                onClick={() => clickNavItem(1)}
+                eventKey="1"
+              >
+                Template View
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <div className="d-flex align-items-center">
+            <Button size="sm" href="https://v2dev.designsystem.gov.sg/" target="_blank">
+              SGDS Docs
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
+      <Body display={active} />
+      <FooterComponent />
+    </Provider>
+  );
 }
 
 export default App;
--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
 // import elements
 import ComponentList from "../componentList/componentList";
 import Template from "../template/template";
 import Sidebar from "../sideBar/sideBar";
 
-class Body extends Component {
-  state = {};
-
-  render() {
-    return (
-      <div className="appBody content-page">
-        {this.props.display === 0 ? <ComponentList /> : <Template />}
-        <Sidebar />
-      </div>
-    );
-  }
+const Body = (props) => {
+  return (
+    <div className="appBody content-page">
+      {props.display === 0 ? <ComponentList /> : <Template />}
+      <Sidebar />
+    </div>
+  );
 }
 
 export default Body;
