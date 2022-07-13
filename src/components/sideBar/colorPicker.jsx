4b4c52ad23eb9d9293759aa744e95ff9705fa15a
Sophia Chow <sophia.chow.2020@scis.smu.edu.sg>
2022-07-13 16:05:42 +0800

    convert colorpicker to functional component

--- src/components/sideBar/colorPicker.jsx
+++ src/components/sideBar/colorPicker.jsx
@@ -1,82 +1,81 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 // Import Color Picker
 import { SketchPicker} from "react-color";
 import { readability } from 'tinycolor2';
 
-class ColorPicker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showColorPicker: false };
+const ColorPicker = (props) => {
+  const [showColorPicker, setShowColorPicker] = useState(false)
+
+  const toggleColorPicker = () => {
+    setShowColorPicker(!showColorPicker)
   }
-  toggleColorPicker() {
-    this.setState({ showColorPicker: !this.state.showColorPicker });
-  }
-  hideColorPicker() {
-    this.setState({ showColorPicker: false });
+
+  const hideColorPicker = () => {
+    setShowColorPicker(false)
   }
 
   //TODO change individual button font color
-  compareColorName(colorName){
-    if(colorName === 'Warning Color' || colorName === 'Table Head Color'){
+  const compareColorName = (colorName) => {
+    if(colorName === 'Warning Color'){
       return  '#0f0f0f';
     }else{
       return '#fff';
     }
   }
   
-  colorContrast(inputColor, colorName){
-    return readability(inputColor, this.compareColorName(colorName) ).toFixed(2);
+  const colorContrast = (inputColor, colorName) => {
+    return readability(inputColor, compareColorName(colorName) ).toFixed(2);
   }
 
-  render() {
-    const popover = {
-      position: "absolute",
-      zIndex: "2"
-    };
-    const cover = {
-      position: "fixed",
-      top: "0px",
-      right: "0px",
-      bottom: "0px",
-      left: "0px"
-    };
-    return (
-      <div className="colorPicker">
-        <div className="fw-bold">{this.props.colorName}
-          <small className={this.colorContrast(this.props.inputColor, this.props.colorName) < 4.5 ? 'ps-2 text-danger' : 'ps-2 text-success' }>
-            {this.colorContrast( this.props.inputColor, this.props.colorName)}
-          </small>
-        </div>
-        <div className="colorPickerContainer"  onClick={() => this.toggleColorPicker()}>
-          <input
-            className="col color me-2"
-            type="button"
-            value={this.props.inputColor}
-            readOnly
-            style={{cursor: "pointer"}}
-          />
-          <div
-            className="color-preview"
-            style={{ backgroundColor: this.props.inputColor }}
-          />
-        </div>
-        
-        {this.state.showColorPicker ? (
-          <div style={popover}>
-            <div style={cover} onClick={() => this.hideColorPicker()} />
-            <SketchPicker
-              color={this.props.inputColor}
-              onChangeComplete={this.props.changeColor}
-              disableAlpha={true}
-              presetColors={[]}
-            />
-          </div>
-        ) : null}
-        
+  const popover = {
+    position: "absolute",
+    zIndex: "2"
+  };
+
+  const cover = {
+    position: "fixed",
+    top: "0px",
+    right: "0px",
+    bottom: "0px",
+    left: "0px"
+  };
+
+  return (
+    <div className="colorPicker">
+      <div className="fw-bold">{props.colorName}
+        <small className={colorContrast(props.inputColor, props.colorName) < 4.5 ? 'ps-2 text-danger' : 'ps-2 text-success' }>
+          {colorContrast( props.inputColor, props.colorName)}
+        </small>
       </div>
-    );
-  }
+      <div className="colorPickerContainer"  onClick={() => toggleColorPicker()}>
+        <input
+          className="col color me-2"
+          type="button"
+          value={props.inputColor}
+          readOnly
+          style={{cursor: "pointer"}}
+        />
+        <div
+          className="color-preview"
+          style={{ backgroundColor: props.inputColor }}
+        />
+      </div>
+      
+      {showColorPicker ? (
+        <div style={popover}>
+          <div style={cover} onClick={() => hideColorPicker()} />
+          <SketchPicker
+            color={props.inputColor}
+            onChangeComplete={props.changeColor}
+            disableAlpha={true}
+            presetColors={[]}
+          />
+        </div>
+      ) : null}
+      
+    </div>
+  );
 }
 export default ColorPicker;
--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -9,21 +9,6 @@ import elementList from "../../elementList/elementList";
 // Import Elements
 import ColorPicker from "./colorPicker";
 
-// Import Redux Store
-import {
-  updateThemePrimaryColor,
-  updateThemeSecondaryColor,
-  updateThemeInfoColor,
-  updateThemeSuccessColor,
-  updateThemeDangerColor,
-  updateThemeWarningColor,
-  updateTableHeadColor,
-  updateTableHoverable,
-  updateTableFullwidth,
-  updateTableNarrow,
-} from "../../store/actions/index";
-// import { statement } from "@babel/template";
-
 import { HelpModal, HelpModalToggle } from "./help";
 
 import { Button } from "@govtechsg/sgds-react"
@@ -33,78 +18,9 @@ class Sidebar extends Component {
     super(props);
     this.state = {
       showElement: elementList[0],
-      // Tables
-      tableHeadColor: cssPresets.tablePresets.tableHeadColor,
-      tableIsNarrow: cssPresets.tablePresets.tableIsNarrow,
-      tableIsHoverable: cssPresets.tablePresets.tableIsHoverable,
-      tableIsFullwidth: cssPresets.tablePresets.tableIsFullwidth,
       showHelpModal: false,
     };
   }
-
-  handleTableHeadColorUpdate = (color) => {
-    try {
-      this.props.updateTableHeadColor(color.hex);
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  };
-
-  handleTableIsHoverableUpdate = (event) => {
-    const value =
-      event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-    this.setState(
-      {
-        tableIsHoverable: value,
-      },
-      () => this.props.updateTableHoverable(value)
-    );
-  };
-
-  handleTableIsFullwidthUpdate = (event) => {
-    const value =
-      event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-    this.setState(
-      {
-        tableIsFullwidth: value,
-      },
-      () => this.props.updateTableFullwidth(value)
-    );
-  };
-
-  handleTableIsNarrow = (event) => {
-    const value =
-      event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-    this.setState(
-      {
-        tableIsNarrow: value,
-      },
-
-      () => this.props.updateTableNarrow(value)
-    );
-  };
-
-  renderSetTableHeadColorDefault = () => {
-    if (
-      this.props.tableHeadColor !==
-      cssPresets.tablePresets.tableHeadColorDefault
-    ) {
-      return (
-        <div
-          className="col is-12 resetBtn"
-          onClick={() => this.resetTableHeadColerDefault()}
-        >
-          <a className="is-block margin-bottom">Reset to default</a>
-        </div>
-      );
-    }
-  };
 
   handleColorChange = (color, colorCat) => {
     if (colorCat === "prcolor") {
@@ -213,83 +129,10 @@ class Sidebar extends Component {
     }
   };
 
-  resetTableHeadColerDefault = () => {
-    try {
-      this.props.updateTableHeadColor(
-        cssPresets.tablePresets.tableHeadColorDefault
-      );
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  };
 
   //Export css/scss
   // Export style
   exportStyle = (exportcss) => {
-    // var buttonLarge =
-    // 	".button-large \r\n { \r\n\t padding-left: " +
-    // 	this.props.buttonLargeHorizontalPadding +
-    // 	"px; \r\n\t padding-right: " +
-    // 	this.props.buttonLargeHorizontalPadding +
-    // 	"px;  \r\n } \r\n";
-    // var buttonMedium =
-    // 	".button-medium  \r\n { \r\n\t padding-left: " +
-    // 	this.props.buttonMediumHorizontalPadding +
-    // 	"px; \r\n\t padding-right: " +
-    // 	this.props.buttonMediumHorizontalPadding +
-    // 	"px; \r\n } \r\n";
-    // var buttonSmall =
-    // 	".button-small \r\n { \r\n\t padding-left: " +
-    // 	this.props.buttonSmallHorizontalPadding +
-    // 	"px; \r\n\t padding-right: " +
-    // 	this.props.buttonSmallHorizontalPadding +
-    // 	"px; \r\n } \r\n";
-
-    // // const displayFontsize = this.props.displayFontsize;
-    // // const smallFontsize = this.props.smallFontsize;
-    // // const tableHeadColor = this.props.tableHeadColor;
-    // // const themePrimaryColor = this.props.themePrimaryColor;
-
-    // var displayFontSize = "";
-    // if (
-    // 	cssPresets.typographyPresets.displayFontSize !==
-    // 	this.props.displayFontsize
-    // ) {
-    // 	displayFontSize =
-    // 		"display \r\n { \r\n\t font-size: " +
-    // 		this.props.displayFontsize +
-    // 		"px; \r\n } \r\n";
-    // }
-
-    // var h1fontSize =
-    // 	"h1 \r\n { \r\n\t font-size: " +
-    // 	this.props.h1Fontsize +
-    // 	"px; \r\n } \r\n";
-    // var h2fontSize =
-    // 	"h2 \r\n { \r\n\t font-size: " +
-    // 	this.props.h2Fontsize +
-    // 	"px; \r\n } \r\n";
-    // var h3fontSize =
-    // 	"h3 \r\n { \r\n\t font-size: " +
-    // 	this.props.h3Fontsize +
-    // 	"px; \r\n } \r\n";
-    // var h4fontSize =
-    // 	"h4 \r\n { \r\n\t font-size: " +
-    // 	this.props.h4Fontsize +
-    // 	"px; \r\n } \r\n";
-    // var h5fontSize =
-    // 	"h5 \r\n { \r\n\t font-size: " +
-    // 	this.props.h5Fontsize +
-    // 	"px; \r\n } \r\n";
-    // var h6fontSize =
-    // 	"h6 \r\n { \r\n\t font-size: " +
-    // 	this.props.h6Fontsize +
-    // 	"px; \r\n } \r\n";
-    // var smallFontsize =
-    // 	"small \r\n { \r\n\t font-size: " +
-    // 	this.props.smallFontsize +
-    // 	"px; \r\n } \r\n";
-
     let exportPrimaryColor =
       "$primary: " + this.props.themePrimaryColor + ";\r\n";
     let exportSecondaryColor =
@@ -306,19 +149,6 @@ class Sidebar extends Component {
       exportSuccessColor +
       exportDangerColor +
       exportWarningColor;
-    // buttonSmall +
-    // buttonMedium +
-    // buttonLarge +
-    // displayFontSize +
-    // h1fontSize +
-    // h2fontSize +
-    // h3fontSize +
-    // h4fontSize +
-    // h5fontSize +
-    // h6fontSize +
-    // smallFontsize;
-
-    console.log(style);
 
     var data = new Blob([style], { type: "text/css" });
     var csvURL = window.URL.createObjectURL(data);
@@ -437,14 +267,6 @@ class Sidebar extends Component {
               </div>
             )}
           </div>
-          {/* <div className="colorpickerCon">
-            <ColorPicker
-              colorName="Table Head Color"
-              inputColor={this.props.tableHeadColor}
-              changeColor={this.handleTableHeadColorUpdate}
-            />
-            {this.renderSetTableHeadColorDefault()}
-          </div> */}
         </div>
       );
     }
@@ -455,92 +277,6 @@ class Sidebar extends Component {
     if (this.state.showElement === elementList[1]) {
       return (
         <div />
-        // <div className="buttonGroup">
-        //   <div className="col is-12 inputTitle">
-        //     $is-small padding left/right
-        //   </div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.smallButtonPaddingHorizontal}
-        //       onChange={this.handleSmallButtonHorizontalPaddingUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetSmallBtnHorizontalPaddingDefault()}
-        //   <div className="col is-12 inputTitle">
-        //     $is-small padding top/bottom
-        //   </div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.smallButtonPaddingVertical}
-        //       onChange={this.handleSmallButtonVerticalPaddingUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetSmallBtnVerticalPaddingDefault()}
-        //   <div className="col is-12 inputTitle">
-        //     $is-medium padding left/right
-        //   </div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.mediumButtonPaddingHorizontal}
-        //       onChange={this.handleMediumButtonHorizontalPaddingUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetMediumBtnHorizontalPaddingDefault()}
-        //   <div className="col is-12 inputTitle">
-        //     $is-medium padding top/bottom
-        //   </div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.mediumButtonPaddingVertical}
-        //       onChange={this.handleMediumButtonVerticalPaddingUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetMediumBtnVerticalPaddingDefault()}
-        //   <div className="col is-12 inputTitle">
-        //     $is-large padding left/right
-        //   </div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.largeButtonPaddingHorizontal}
-        //       onChange={this.handleLargeButtonHorizontalPaddingUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetLargeBtnHorizontalPaddingDefault()}
-        //   <div className="col is-12 inputTitle">
-        //     $is-large padding top/bottom
-        //   </div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.largeButtonPaddingVertical}
-        //       onChange={this.handleLargeButtonVerticalPaddingUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetLargeBtnVerticalPaddingDefault()}
-        // </div>
       );
     }
   };
@@ -550,104 +286,6 @@ class Sidebar extends Component {
     if (this.state.showElement === elementList[2]) {
       return (
         <div />
-        // <div className="buttonGroup">
-        //   <div className="col is-12 inputTitle">$display font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.displayFontsize}
-        //       onChange={this.handleDisplayFontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetDisplayFontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$h1 font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.h1Fontsize}
-        //       onChange={this.handleH1FontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetH1FontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$h2 font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.h2Fontsize}
-        //       onChange={this.handleH2FontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetH2FontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$h3 font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.h3Fontsize}
-        //       onChange={this.handleH3FontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetH3FontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$h4 font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.h4Fontsize}
-        //       onChange={this.handleH4FontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetH4FontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$h5 font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.h5Fontsize}
-        //       onChange={this.handleH5FontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetH5FontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$h6 font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.h6Fontsize}
-        //       onChange={this.handleH6FontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetH6FontsizeDefault()}
-        //   <div className="col is-12 inputTitle">$small font-size</div>
-        //   <div className="col is-12">
-        //     <input
-        //       className="col is-11 inputField"
-        //       type="text"
-        //       value={this.state.smallFontsize}
-        //       onChange={this.handleSmallFontsizeUpdate}
-        //       name=""
-        //       id=""
-        //     />
-        //   </div>
-        //   {this.renderSetSmallFontsizeDefault()}
-        // </div>
       );
     }
   };
@@ -709,50 +347,8 @@ class Sidebar extends Component {
   };
 
   render() {
-    // const styleProperties = {
-    //   color: "#6037B3",
-    //   fontWeight: "600"
-    // };
     return (
       <div className="sideBar">
-        {/* <div className="elementList">
-          <div
-            className="listItem"
-            style={
-              this.state.showElement === elementList[0] ? styleProperties : null
-            }
-            onClick={() => this.toggleElement(elementList[0])}
-          >
-            <a>Colours</a>
-          </div>
-          <div
-            className="listItem"
-            style={
-              this.state.showElement === elementList[1] ? styleProperties : null
-            }
-            onClick={() => this.toggleElement(elementList[1])}
-          >
-            <a>Buttons</a>
-          </div>
-          <div
-            className="listItem"
-            style={
-              this.state.showElement === elementList[2] ? styleProperties : null
-            }
-            onClick={() => this.toggleElement(elementList[2])}
-          >
-            <a>Typography</a>
-          </div>
-          <div
-            className="listItem"
-            style={
-              this.state.showElement === elementList[3] ? styleProperties : null
-            }
-            onClick={() => this.toggleElement(elementList[3])}
-          >
-            <a>Tables</a>
-          </div>
-        </div> */}
         <div className="elements">
           {this.renderGlobalProperties()}
           {this.renderButtonGroup()}
@@ -800,27 +396,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    updateThemePrimaryColor: (themeSecondaryColor) =>
-      dispatch(updateThemePrimaryColor(themeSecondaryColor)),
-    updateThemeSecondaryColor: (themePrimaryColor) =>
-      dispatch(updateThemeSecondaryColor(themePrimaryColor)),
-    updateThemeInfoColor: (infoColor) =>
-      dispatch(updateThemeInfoColor(infoColor)),
-    updateThemeSuccessColor: (successColor) =>
-      dispatch(updateThemeSuccessColor(successColor)),
-    updateThemeDangerColor: (dangerColor) =>
-      dispatch(updateThemeDangerColor(dangerColor)),
-    updateThemeWarningColor: (warningColor) =>
-      dispatch(updateThemeWarningColor(warningColor)),
-    updateTableHeadColor: (color) => dispatch(updateTableHeadColor(color)),
-    updateTableNarrow: (isNarrow) => dispatch(updateTableNarrow(isNarrow)),
-    updateTableHoverable: (isHoverable) =>
-      dispatch(updateTableHoverable(isHoverable)),
-    updateTableFullwidth: (isFullWidth) =>
-      dispatch(updateTableFullwidth(isFullWidth)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+export default connect(mapStateToProps)(Sidebar);
